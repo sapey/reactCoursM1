fba09c6e4f5992111114daa9cd12b41223b8f32b
sapey <alexandre.froment92@gmail.com>
2022-02-03 11:06:54 +0100

    premier composant

---
+++ jour1-tp/src/App.js
@@ -0,0 +1,8 @@
+import './App.css';
+import Premier from './composants/Premier';
+
+function App() {
+  return <Premier />
+}
+
+export default App;
---
+++ jour1-tp/src/composants/Premier.jsx
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+class Premier extends Component {
+    render(){
+        return <p>Voici notre premier composant</p>
+    }
+}
+
+export default Premier;
