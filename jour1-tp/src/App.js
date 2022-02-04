939e8a850b0fe4d8df579c784173952aafbb4ff7
sapey <alexandre.froment92@gmail.com>
2022-02-04 09:10:58 +0100

    last jour 1

--- jour1-tp/src/App.js
+++ jour1-tp/src/App.js
@@ -12,9 +12,19 @@ import { Exo3 } from './composants/Exo3';
 import { StateF } from './composants/StateF';
 import { StateC } from './composants/StateC';
 import { StateF2 } from './composants/StateF2';
+import { StateF3 } from './composants/StateF3';
+import { Exo4 } from './composants/Exo4';
+import { Img } from './composants/Img';
+import { A } from './composants/A';
 
 function App() {
   return <>
+      <A source="http" couleur="rouge" />
+      <div>
+        <Img />
+      </div> 
+      <Exo4 />
+      <StateF3 />
       <StateF2 />
       <StateC />
       <StateF />
